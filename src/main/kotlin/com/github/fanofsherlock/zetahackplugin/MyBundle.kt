bdb98e71d79418e21670a38c7309b382fc60b68b
GitHub Action <action@github.com>
2021-02-20 18:22:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/fanofsherlock/zetahackplugin/MyBundle.kt
+++ src/main/kotlin/com/github/fanofsherlock/zetahackplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.fanofsherlock.zetahackplugin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/fanofsherlock/zetahackplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.fanofsherlock.zetahackplugin.services
+
+import com.github.fanofsherlock.zetahackplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
